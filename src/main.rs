09e217bf68dcc2706a0b6fb1c42ab3ba7caf5ef6
Eric D. Reichert <eric@reichertconsulting.com>
2016-06-30 13:31:21 -0400

    Opened the media file and found the stream info.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ fn main() {
         ffmpeg_sys::avformat_network_init();
 
         let mut src_avfc = AVFC::new();
-        if let Ok(c_src_uri) = ffi::CString::new(src_uri.to_string()) {
+        let possible_c_src_uri = ffi::CString::new(src_uri.to_string());
+        if let Ok(ref c_src_uri) = possible_c_src_uri {
             // Open video file
             if ffsys::avformat_open_input(&mut src_avfc.0, c_src_uri.as_ptr(), ptr::null(), ptr::null_mut()) != 0 {
                 println!("Could not open media file {}", src_uri);
@@ -25,6 +26,35 @@ fn main() {
             }
         } else {
             println!("Could not convert the source URI to a C String.");
+            process::exit(-1);
+        }
+
+        println!("Opened media file {}.", src_uri);
+
+        // Retrieve stream information
+        if ffsys::avformat_find_stream_info(src_avfc.0, ptr::null_mut()) < 0 {
+            println!("Could not find the stream info.");
+            process::exit(-1);
+        }
+        println!("Found stream info for {}.", src_uri);
+
+        // Dump information about file
+        ffsys::av_dump_format(src_avfc.0, 0, possible_c_src_uri.unwrap().as_ptr(), 0);
+
+        // Find the first video stream
+        let mut stream_idx = 0;
+        let avfc_deref = &*src_avfc.0;
+        let num_streams = avfc_deref.nb_streams;
+        let streams = avfc_deref.streams;
+        if let Some(idx) = (0..num_streams as u32).find(|x| {
+            let av_stream_deref = &**streams.offset(*x as isize);
+            let av_codec_ctx_deref = &*av_stream_deref.codec;  
+            av_codec_ctx_deref.codec_type == ffsys::AVMEDIA_TYPE_VIDEO
+        }) {
+            println!("Found video stream at stream index {}", idx);
+            stream_idx = idx;
+        } else {
+            println!("Could not a video stream.");
             process::exit(-1);
         }
     }
