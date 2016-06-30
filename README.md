4f9cac872c8a2ea07c374d4aa181ab9477970452
Eric D. Reichert <eric@reichertconsulting.com>
2016-06-30 13:15:02 -0400

    Updated the readme.  Started working through tutorial 1.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-An experimentation project, based on the Dranger FFMPEG tutorials http://dranger.com/ffmpeg/, ported to Rust.
+An experimentation project, loosley based on the Dranger FFMPEG tutorials http://dranger.com/ffmpeg/ ported to Rust.
 
-It's mostly an easy way to test ideas for working with FFMPEG in Rust without a bunch of stuff in the way. 
+It's an easy way to test ideas for working with FFMPEG in Rust without a bunch of stuff in the way. 
+
+It includes some light abstraction but a production application would likely have more to make use of rust-ffmpeg-sys safer.
+
+For an example of a safe wrapper around rust-ffmpeg-sys see https://github.com/meh/rust-ffmpeg.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+extern crate ffmpeg_sys;
+
+use std::{ptr, process, env, ffi};
+use ffmpeg_sys as ffsys;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please specify a media file.");
+        process::exit(-1);
+    }
+    let ref src_uri = args[1];
+    unsafe {
+        // Register all formats, codecs, and network features.
+        ffsys::av_register_all();
+        ffmpeg_sys::avformat_network_init();
+
+        let mut src_avfc = AVFC::new();
+        if let Ok(c_src_uri) = ffi::CString::new(src_uri.to_string()) {
+            // Open video file
+            if ffsys::avformat_open_input(&mut src_avfc.0, c_src_uri.as_ptr(), ptr::null(), ptr::null_mut()) != 0 {
+                println!("Could not open media file {}", src_uri);
+                process::exit(-1);
+            }
+        } else {
+            println!("Could not convert the source URI to a C String.");
+            process::exit(-1);
+        }
+    }
+
+    process::exit(0);
+}
+
+struct AVFC(*mut ffsys::AVFormatContext);
+
+impl AVFC {
+
+    fn new() -> AVFC {
+        AVFC(ptr::null_mut())
+    }
+}
+
+impl Drop for AVFC {
+    
+    fn drop(&mut self) -> () {
+        if !self.0.is_null() {
+            unsafe {
+                println!("Dropping the AVFC.");
+                ffsys::avformat_close_input(&mut self.0);
+            }
+        }
+    }
 }
