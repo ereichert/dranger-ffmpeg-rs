4cee2ca6d1250ddfea0d0cb230f292bd9a5c99f6
Eric D. Reichert <eric@reichertconsulting.com>
2016-07-01 14:23:50 -0400

    Added the encoding to JPEG and write to the file system.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate alloc_system;
 
 extern crate ffmpeg_sys;
 
-use std::{ptr, process, env, ffi, mem};
+use std::{ptr, process, env, ffi, mem, fs, slice};
 use ffmpeg_sys as ffsys;
+use std::io::Write;
 
 fn main() {
     println!("Starting");
@@ -63,12 +64,12 @@ fn main() {
 
         // Get a pointer to the codec context for the video stream
         let stream = &**streams.offset(stream_idx as isize);
-        let avcc = AVCC::new(stream.codec);
+        let avcc = AVCC::wrap(stream.codec);
 
         println!("Retrieved AVCodecContext for {}.", src_uri);
 
         // Find the decoder for the video stream
-        let codec_id = (*avcc.0).codec_id;
+        let codec_id = (*avcc.avcc).codec_id;
         let avc = ffsys::avcodec_find_decoder(codec_id);
         if avc.is_null() {
             println!("Could not find a codec for codec id {:?}.", codec_id);
@@ -78,7 +79,7 @@ fn main() {
         println!("Found codec {:?}.", ffi::CStr::from_ptr((*avc).name));
 
         // Open codec
-        if ffsys::avcodec_open2(avcc.0, avc, ptr::null_mut()) < 0 {
+        if ffsys::avcodec_open2(avcc.avcc, avc, ptr::null_mut()) < 0 {
             println!("Could not open the codec context for {}.", src_uri);
             process::exit(-1);
         } 
@@ -93,12 +94,12 @@ fn main() {
             // Is this a packet from the video stream?
             if packet.stream_index() == stream_idx {
                 // Decode video frame
-                ffsys::avcodec_decode_video2(avcc.0, src_frame.as_mut_ptr(), &mut frame_finished, packet.as_ptr());
+                ffsys::avcodec_decode_video2(avcc.avcc, src_frame.as_mut_ptr(), &mut frame_finished, packet.as_ptr());
                 // Did we get a video frame?
                 if frame_finished != 0 {
                     println!("Saving frame {} to disk.", frame_num + 1);
                     // Save the frame to disk
-                    // avframe_to_jpeg(src_frame, frame_num + 1);
+                    save_avframe_to_jpeg(&src_frame, frame_num + 1);
                     frame_num += 1;
                 }
             }
@@ -109,6 +110,73 @@ fn main() {
     }
 
     process::exit(0);
+}
+
+unsafe fn save_avframe_to_jpeg(src_frame: &AVF, frame_num: i32) -> i32 {
+    let mjpeg_codec = ffsys::avcodec_find_encoder(ffsys::AV_CODEC_ID_MJPEG);
+    if mjpeg_codec.is_null() {
+        println!("could not find mjpeg codec");
+        return -1;
+    }
+
+    println!("Found MJPEG codec.");
+
+    let jpeg_avcc = AVCC::new(mjpeg_codec);
+    if jpeg_avcc.avcc.is_null() {
+        println!("could not allocate jpeg codec context");
+        return -1;
+    }
+
+    println!("Allocated the MJPEG codec context.");
+
+    let ref mut jpeg_avcc_deref = *jpeg_avcc.avcc;
+    let ref src_frame_deref = *src_frame.0;
+    jpeg_avcc_deref.pix_fmt = ffsys::AV_PIX_FMT_YUVJ420P;
+    jpeg_avcc_deref.height = src_frame_deref.height;
+    jpeg_avcc_deref.width = src_frame_deref.width;
+    jpeg_avcc_deref.time_base.num = 1;
+    jpeg_avcc_deref.time_base.den = 1000000;
+
+    println!("height = {}", src_frame_deref.height);
+    println!("width = {}", src_frame_deref.width);
+
+    println!("before opening the jpeg context");
+    if ffsys::avcodec_open2(jpeg_avcc.avcc, mjpeg_codec, ptr::null_mut()) < 0 {
+        println!("could not open jpeg codec context");
+        return -1;
+    }
+
+    let mut packet = AVP::new_with_size(src_frame_deref.height * src_frame_deref.width);
+
+    let mut frame_complete = 0;
+    println!("before encoding the jpeg");
+    if ffsys::avcodec_encode_video2(jpeg_avcc.avcc, packet.as_mut_ptr(), src_frame.as_ptr(), &mut frame_complete) < 0 {
+        println!("could not encode jpeg");
+        return -1;
+    }
+
+    let file_open_result = fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(format!("sample-{}.jpg", frame_num));
+
+    if file_open_result.is_err() {
+        println!("failed to open file for writing the JPEG");
+        return -1;
+    }
+
+    let mut file = file_open_result.unwrap();
+    println!("Opened file for writing.");
+        
+    let data = slice::from_raw_parts(packet.0.data as *const u8, packet.0.size as usize);
+    let write_result = file.write(data);
+    
+    if write_result.is_err() {
+        println!("failed to write the JPEG");
+        return -1;
+    }
+    
+    0
 }
 
 struct AVFC(*mut ffsys::AVFormatContext);
@@ -132,22 +200,39 @@ impl Drop for AVFC {
     }
 }
 
-struct AVCC(*mut ffsys::AVCodecContext);
+struct AVCC{
+    avcc: *mut ffsys::AVCodecContext,
+    wrapped: bool,
+}
 
 impl AVCC {
 
-    fn new(avcc: *mut ffsys::AVCodecContext) -> AVCC {
-        AVCC(avcc)
+    unsafe fn new(avc: *mut ffsys::AVCodec) -> AVCC {
+        AVCC {
+            avcc: ffsys::avcodec_alloc_context3(avc),
+            wrapped: false,
+        }
+    }
+
+    fn wrap(avcc: *mut ffsys::AVCodecContext) -> AVCC {
+        AVCC {
+            avcc: avcc,
+            wrapped: true,
+        }
     }
 }
 
 impl Drop for AVCC {
 
     fn drop(&mut self) -> () {
-        if !self.0.is_null() {
+        if !self.avcc.is_null() {
             unsafe {
                 println!("Dropping the AVCC.");
-                ffsys::avcodec_close(self.0);
+                if self.wrapped {
+                    ffsys::avcodec_close(self.avcc);
+                } else {
+                    ffsys::avcodec_free_context(&mut self.avcc);
+                }
             }
         }
     }
@@ -157,11 +242,13 @@ pub struct AVF(*mut ffsys::AVFrame);
 
 impl AVF {
 
-    pub fn new() -> AVF {
-        unsafe {
-            let av_frame = ffsys::av_frame_alloc();
-            AVF(av_frame)
-        }
+    pub unsafe fn new() -> AVF {
+        let av_frame = ffsys::av_frame_alloc();
+        AVF(av_frame)
+    }
+
+    pub fn as_ptr(&self) -> *const ffsys::AVFrame {
+        self.0
     }
 
     pub fn as_mut_ptr(&mut self) -> *mut ffsys::AVFrame {
@@ -183,12 +270,16 @@ pub struct AVP(ffsys::AVPacket);
 
 impl AVP {
 
-    pub fn new() -> AVP {
-        unsafe {
-            let mut pkt: ffsys::AVPacket = mem::zeroed();
-            ffsys::av_init_packet(&mut pkt);
-            AVP(pkt)
-        }
+    pub unsafe fn new() -> AVP {
+        let mut pkt: ffsys::AVPacket = mem::zeroed();
+        ffsys::av_init_packet(&mut pkt);
+        AVP(pkt)
+    }
+
+    pub unsafe fn new_with_size(size: i32) -> AVP {
+        let mut pkt: ffsys::AVPacket = mem::zeroed();
+        ffsys::av_new_packet(&mut pkt, size);
+        AVP(pkt)
     }
 
     pub fn as_ptr(&self) -> *const ffsys::AVPacket {
@@ -203,17 +294,17 @@ impl AVP {
         self.0.stream_index
     }
 
-    pub fn free(&mut self) -> () {
-        unsafe {
-            println!("Dropping the AVP.");
-            ffsys::av_packet_unref(self.as_mut_ptr());
-        }
+    pub unsafe fn free(&mut self) -> () {
+        println!("Dropping the AVP.");
+        ffsys::av_packet_unref(self.as_mut_ptr());
     }
 }
 
 impl Drop for AVP {
     
     fn drop(&mut self) -> () {
-        self.free()
+        unsafe {
+            self.free()
+        }
     }
 }
